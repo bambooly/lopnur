96b840f5677c11589000c82a12788d371c5fa674
duanjunyi <junyi_duan@126.com>
2021-01-09 07:01:28 +0800

    delete compiler generated files

--- target/rls/debug/deps/lopnur-c61d30d32f3a4a44.d
+++
@@ -1,5 +0,0 @@
-/home/duanjunyi/git/init/lopnur/target/rls/debug/deps/lopnur-c61d30d32f3a4a44.rmeta: src/lib.rs
-
-/home/duanjunyi/git/init/lopnur/target/rls/debug/deps/lopnur-c61d30d32f3a4a44.d: src/lib.rs
-
-src/lib.rs:
